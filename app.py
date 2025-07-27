656947f2b76de7dc8e1f4a2c905a13894e4fa112
nikhil kumar <nikhilkumarsingh5872@gmail.com>
2025-07-27 15:09:50 +0530

    updated app file

--- app.py
+++ app.py
@@ -13,7 +13,12 @@ load_dotenv()
 
 import pymongo
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, File, UploadFile,Request
+from fastapi import (
+    FastAPI, 
+    File, 
+    UploadFile,
+    Request
+)
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
@@ -32,6 +37,7 @@ from Network_Security.constants.training_pipeline import (
     DATA_INGESTION_COLLECTION_NAME,
     DATA_INGESTION_DATABASE_NAME
 )
+from Network_Security.utils.ml_utils.model.estimator import NetworkModel
 
 
 
@@ -55,6 +61,12 @@ app.add_middleware(
 )
 
 
+
+from fastapi.templating import Jinja2Templates
+templates= Jinja2Templates(directory="./templates")
+
+
+
 @app.get("/",tags=["authentication"])
 async def index():
     return RedirectResponse(url="/docs")
@@ -70,10 +82,51 @@ async def train_route():
 
         return Response("training is successful")
 
-    except Exception as e:
+    except Exception as e:   
         raise NetworkSecurityException(e,sys)
     
 
+
+
+@app.post("/predict")
+async def predict_route(request:Request,file:UploadFile=File(...)):
+    try:
+        input_file_path = "valid_data/input.csv"
+        output_file_path = "valid_data/output.csv"
+        preprocessor_path= "final_model/preprocessor.pkl"
+        model_path= "final_model/model.pkl"
+
+        df=pd.read_csv(input_file_path)
+        preprocessor=load_object(preprocessor_path)
+        model=load_object(model_path)
+
+        network_model_object=NetworkModel(
+            preprocessor=preprocessor,
+            model=model
+        )
+
+        y_pred=network_model_object.predict(df)
+        
+        df["predicted_column"] = y_pred
+
+        df.to_csv(
+            output_file_path,
+            index=False,
+            header=True
+        )
+
+        table_html = df.to_html(classes="table table-striped")
+
+        return templates.TemplateResponse(
+            "table.html",
+            {
+                "request":request,
+                "table":table_html
+            }
+        )
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
 
 
 if __name__=="__main__":
